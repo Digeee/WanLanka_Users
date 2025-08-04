5ce46f2928feb56311fd338e2010164f34b07b1e
Digeee <digee12@gmail.com>
2025-08-05 00:10:02 +0530

    digee header and slider change

---
+++ resources/views/components/home-slider.blade.php
@@ -0,0 +1,36 @@
+<div class="c">
+    <input type="radio" name="a" id="cr-1" checked>
+    <label for="cr-1"></label>
+    <div class="ci active">
+        <h2 class="ch">What do you know?</h2>
+        <img src="{{ asset('images/slider-image-1.jpg') }}" alt="Snow on leafs">
+    </div>
+
+    <input type="radio" name="a" id="cr-2">
+    <label for="cr-2"></label>
+    <div class="ci">
+        <h2 class="ch">Look from inside?</h2>
+        <img src="{{ asset('images/slider-image-2.jpg') }}" alt="Trees">
+    </div>
+
+    <input type="radio" name="a" id="cr-3">
+    <label for="cr-3"></label>
+    <div class="ci">
+        <h2 class="ch">In the mountains?</h2>
+        <img src="{{ asset('images/slider-image-3.jpg') }}" alt="Mountains and houses">
+    </div>
+
+    <input type="radio" name="a" id="cr-4">
+    <label for="cr-4"></label>
+    <div class="ci">
+        <h2 class="ch">Above looks beautiful?</h2>
+        <img src="{{ asset('images/slider-image-4.jpg') }}" alt="Sky and mountains">
+    </div>
+
+    <div class="slider-nav">
+        <label for="cr-1"></label>
+        <label for="cr-2"></label>
+        <label for="cr-3"></label>
+        <label for="cr-4"></label>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ritz Tours</title>
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/slider.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/slider.js') }}"></script>
 </head>
 <body>
-    @include('include.header')
+</br>
+</br>
+</br>
+</br>
 
+    @include('include.header') <!-- Your header blade component -->
 
+    <!-- Home Slider Component -->
+    @include('components.home-slider')
+
+    <!-- Content goes here -->
+    @yield('content')
 </body>
 </html>
