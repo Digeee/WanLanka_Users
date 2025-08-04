c12f1ef92a770b022c9660e7f057b4d5a101b5e1
Digeee <digee12@gmail.com>
2025-08-05 00:37:34 +0530

    digee header and slider change2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ritz Tours</title>
+    <title>WanLanka</title>
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
     <link href="{{ asset('css/slider.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/home-contact.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
+
     <script src="{{ asset('js/slider.js') }}"></script>
 </head>
 <body>
@@ -21,7 +24,12 @@
     <!-- Home Slider Component -->
     @include('components.home-slider')
 
+    @include('components.home-contact')
+
     <!-- Content goes here -->
     @yield('content')
+
+
+    @include('include.footer') <!-- Your footer blade component -->
 </body>
 </html>
